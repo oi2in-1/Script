63f2506fdc280d26a63a7abe73d8ef6e8e3d1a73
script-fourms <88901537+script-fourms@users.noreply.github.com>
2021-08-18 01:56:33 -0700

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   '*'
+   'https://github.com/script-fourms/Script/tree/master/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
